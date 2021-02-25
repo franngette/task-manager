452935e3dee02cb0f577adae591ecca1edcb80df
Cristian Viterales <cviterales@cooperativabatan.com.ar>
2021-02-25 15:17:21 -0300

    added interceptor

---
+++ src/api/interceptors.js
@@ -0,0 +1,11 @@
+import axiosInstance from "./axios";
+import {AUTH_LOGOUT} from "../store/actions/actionTypes";
+import store from "../store/store";
+
+export default {
+  setupInterceptors: () => {
+    // Add a response interceptor
+    
+
+  },
+};
